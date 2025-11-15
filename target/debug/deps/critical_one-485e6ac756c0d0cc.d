bbe7e66831a031eb7123bccb3d42fadbb2a1bb29
brandon <b.charest019@gmail.com>
2025-11-15 13:40:51 -0500

    init

---
+++ target/debug/deps/critical_one-485e6ac756c0d0cc.d
@@ -0,0 +1,5 @@
+/home/brandon/Coding/Rust/critical-one/target/debug/deps/critical_one-485e6ac756c0d0cc.d: src/main.rs
+
+/home/brandon/Coding/Rust/critical-one/target/debug/deps/libcritical_one-485e6ac756c0d0cc.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/critical_one-64fe14623279f7fd.d
@@ -0,0 +1,5 @@
+/home/brandon/Coding/Rust/critical-one/target/debug/deps/critical_one-64fe14623279f7fd.d: src/main.rs
+
+/home/brandon/Coding/Rust/critical-one/target/debug/deps/libcritical_one-64fe14623279f7fd.rmeta: src/main.rs
+
+src/main.rs:
