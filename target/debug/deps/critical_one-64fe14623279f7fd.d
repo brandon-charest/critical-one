6776c68026c100c64fe90991aa8cc8773b7aba8f
brandon <b.charest019@gmail.com>
2025-11-17 11:46:18 -0500

    gitignore

--- target/debug/deps/critical_one-64fe14623279f7fd.d
+++
@@ -1,5 +0,0 @@
-/home/brandon/Coding/Rust/critical-one/target/debug/deps/critical_one-64fe14623279f7fd.d: src/main.rs
-
-/home/brandon/Coding/Rust/critical-one/target/debug/deps/libcritical_one-64fe14623279f7fd.rmeta: src/main.rs
-
-src/main.rs:
